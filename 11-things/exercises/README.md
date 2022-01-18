0df79a738991f48e137c23c62f52f106a9034fe7
ChihYu Yeh <cyyeh@data-sci.info>
2022-01-18 13:31:40 +0800

    reformat and add exercises description text

---
+++ 11-things/exercises/README.md
@@ -0,0 +1,7 @@
+# Exercises
+
+1. Another language. Implement the example program in another language, but preserve the style.
+2. Forgotten methods. In the example program, the methods info are never invoked. Change the program (to the minimum) so that all of them are invoked, and their results are printed out. What happens internally when that method is invoked on the DataStorageManager object and on the WordFrequencyController object? Explain the results.
+3. A different program. Write a different program, also in the Things style, that does exactly the same thing as the example program, but with different classes, i.e. using a different decomposition of the problem. No need to preserve the relatively artificial inheritance relations of the example, or the info methods.
+4. Classless. Write a Things style implementation of the term frequency problem without using Python’s classes. No need to preserve the info methods.
+5. A different task. Write one of the tasks proposed in the Prologue using this style.
--- 11-things/tf-11.py
+++ 11-things/tf-11.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
-import sys, re, operator, string
+import sys
+import re
+import operator
+import string
 from abc import ABCMeta
+
 
 #
 # The classes
@@ -11,13 +15,14 @@ class TFExercise():
     def info(self):
         return self.__class__.__name__
 
+
 class DataStorageManager(TFExercise):
     """ Models the contents of the file """
-    
+
     def __init__(self, path_to_file):
         with open(path_to_file) as f:
             self._data = f.read()
-        pattern = re.compile('[\W_]+')
+        pattern = re.compile(r'[\W_]+')
         self._data = pattern.sub(' ', self._data).lower()
 
     def words(self):
@@ -25,11 +30,16 @@ class DataStorageManager(TFExercise):
         return self._data.split()
 
     def info(self):
-        return super(DataStorageManager, self).info() + ": My major data structure is a " + self._data.__class__.__name__
+        return (
+            super(DataStorageManager, self).info() +
+            ": My major data structure is a " +
+            self._data.__class__.__name__
+        )
+
 
 class StopWordManager(TFExercise):
     """ Models the stop word filter """
-    
+
     def __init__(self):
         with open('../stop_words.txt') as f:
             self._stop_words = f.read().split(',')
@@ -40,11 +50,16 @@ class StopWordManager(TFExercise):
         return word in self._stop_words
 
     def info(self):
-        return super(StopWordManager, self).info() + ": My major data structure is a " + self._stop_words.__class__.__name__
+        return (
+            super(StopWordManager, self).info() +
+            ": My major data structure is a " +
+            self._stop_words.__class__.__name__
+        )
+
 
 class WordFrequencyManager(TFExercise):
     """ Keeps the word frequency data """
-    
+
     def __init__(self):
         self._word_freqs = {}
 
@@ -58,7 +73,12 @@ class WordFrequencyManager(TFExercise):
         return sorted(self._word_freqs.items(), key=operator.itemgetter(1), reverse=True)
 
     def info(self):
-        return super(WordFrequencyManager, self).info() + ": My major data structure is a " + self._word_freqs.__class__.__name__
+        return (
+            super(WordFrequencyManager, self).info() +
+            ": My major data structure is a " +
+            self._word_freqs.__class__.__name__
+        )
+
 
 class WordFrequencyController(TFExercise):
     def __init__(self, path_to_file):
@@ -74,6 +94,7 @@ class WordFrequencyController(TFExercise):
         word_freqs = self._word_freq_manager.sorted()
         for (w, c) in word_freqs[0:25]:
             print(w, '-', c)
+
 
 #
 # The main function
